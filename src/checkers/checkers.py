1666cf342ee669f854ca47c848a0c58aa9789b0c
William Wallius <william.wallius@gmail.com>
2023-01-20 13:44:32 -0800

    save

--- src/checkers/checkers.py
+++ src/checkers/checkers.py
@@ -12,12 +12,35 @@ class Checkers:
             player.player_num = i+1
 
     def get_moves(self, player_num):
-        valid_pieces = []
+        valid_moves = []
         for i in range(8):
             for j in range(8):
-                piece = self.board[i][j]
-                # if piece == player_num:
-                #     if piece > 0:
+                piece_num = self.board[i][j]
+
+                if piece_num == player_num:
+                    if piece_num == 1 or piece_num < 0:
+                            try: #[-1] is valid, need to fix
+                                if i>0 and j>0 and self.board[i-1][j-1] == 0:
+                                    valid_moves.append(((i, j), (-1, -1)))
+                            except: pass
+                            try:
+                                if i>0 and self.board[i-1][j+1] == 0:
+                                    valid_moves.append(((i, j), (-1, 1)))
+                            except: pass
+                    
+                    if piece_num == 2 or piece_num < 0:
+                            try:
+                                if self.board[i+1][j+1] == 0:
+                                    valid_moves.append(((i, j), (1, 1)))
+                            except: pass
+                            try:
+                                if j>0 and self.board[i+1][j-1] == 0:
+                                    valid_moves.append(((i, j), (1, -1)))
+                            except: pass
+        print(valid_moves)
+
+                        
+                        
 
                         
                 #          or player_num == 3:
@@ -53,7 +76,6 @@ class Checkers:
         self.board[move[0][0] + move[1][0]][move[0][1] + move[1][1]] = player.player_num
 
 
-
     
     def run(self, num_turns=250):
         for i in range(1, 2*num_turns):
@@ -64,3 +86,12 @@ class Checkers:
 
             if winner:
                 return winner
+    
+    def print_board(self, board=None):
+        if not board:
+            board = self.board
+        print('   ', *range(8), '\n    ―――――――――')
+        for i, row in enumerate(board):
+            print(i, '|', *row, sep=' ')
+    
+
--- src/checkers/test_checkers.py
+++ src/checkers/test_checkers.py
@@ -8,15 +8,17 @@ from random_player import *
 players = [RandomPlayer(), RandomPlayer()]
 
 game = Checkers(players)
+game.print_board()
+game.get_moves(1)
 
 # print(game.check_winner())
 
 # game.board = [[0,0,0,0,1,2,0,0],[],[],[],[],[],[],[0,0,0,0,1,2,0,0]]
 # game.check_crowns()
-n_board = [row.copy() for row in game.board]
-n_board[0][0] = 100
-print(n_board)
-print(game.board)
+# n_board = [row.copy() for row in game.board]
+# n_board[0][0] = 100
+# print(n_board)
+# print(game.board)
 # print(game.board)
 # game.run()
 # print(game.winner)
